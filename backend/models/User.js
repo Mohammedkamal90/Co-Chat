c45652a653f4a3e8440aa0eab1c5f0863c5a3b02
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-29 23:00:43 +0300

    User Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     bio: { type: String },
-    profilePicture: { type: String }
+    profilePicture: { type: String }  // URL to the profile picture
 });
 
 module.exports = mongoose.model('User', UserSchema);
